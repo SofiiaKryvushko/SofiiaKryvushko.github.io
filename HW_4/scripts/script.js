157746a74d06114328c00ecce142edffda352b08
SofiiaKryvushko <61798232+SofiiaKryvushko@users.noreply.github.com>
2021-09-15 23:20:55 +0300

    Update script.js

--- HW_4/scripts/script.js
+++ HW_4/scripts/script.js
@@ -3,29 +3,29 @@ const age = prompt('Enter age', '18');
 const role = prompt('Enter role');
 const occupation = prompt('Enter occupation');
 
-let menu= prompt('1 - name defined, age>18\n' +
+let menu = prompt('1 - name defined, age>18\n' +
     '2 - role not admin or occupation not ceo\n' +
     '3 - occupation if defined, if not - role\n' +
     '4 - name - defined & age>18 / role - admin\n' +
     '5 - role - not admin & name - defined');
-switch (menu){
+switch (menu) {
 
     case'1':
-        alert((name === 'undefined') && (age > 18));
+        alert((name === 'undefined') && (age > 18));//+
         break;
     case'2':
         alert((role === 'Admin' || role === 'admin') || (occupation === 'CEO' || occupation === 'ceo'));
-        break;
+        break;//+
     case'3':
-        alert(occupation === undefined ? role: occupation);
+        alert(occupation ?? undefined ? occupation: role);//+
         break;
     case'4':
-        alert((((name !== 'undefined') && (age > 18)) || (role === 'admin'||role === 'Admin')));
+        alert((((name ?? undefined) && (age > 18)) || (role === 'admin' || role === 'Admin')));
         break;
     case'5':
-        alert(((role !== 'admin') && (name === 'undefined')) ? age : false);
+        alert(((role !== 'admin') && (name ?? 'undefined')) ? age : false);
         break;
-    default: break;
+    default:
+        break;
 }
-
 
